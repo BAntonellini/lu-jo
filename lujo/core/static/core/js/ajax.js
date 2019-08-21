18deefb39eac565f52a59c2dc855ee66da8bc06d
Bruno <antonellinibruno@gmail.com>
2019-08-21 08:38:12 -0300

    Commitangie

--- lujo/core/static/core/js/ajax.js
+++ lujo/core/static/core/js/ajax.js
@@ -6,8 +6,54 @@ window.onload = function() {
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var json_response = JSON.parse(this.response);
-            console.log(json_response)
+          var json_response = JSON.parse(this.response);
+          console.log(json_response);
+
+          var modal_title = document.querySelector(".modal-title");
+          modal_title.innerHTML = json_response.titulo;
+
+          var modal_body = document.querySelector(".modal-body");
+          modal_body.innerHTML = json_response.resumen;
+
+          var modal_imagen = document.querySelector(".imagen-modal");
+          modal_imagen.setAttribute("src", "media/" + json_response.imagen);
+
+          var boton_registrar_curso = document.querySelector(
+            "#boton-registrar-curso"
+          );
+          boton_registrar_curso.addEventListener("click", function(e) {
+            console.log(e.target);
+            window.location.href = "#contact-form";
+
+            
+            var dia = new Date();
+            var hora = dia.getHours();
+            var saludo = "";
+            switch (true) {
+              case (hora >= 0 && hora <= 5):
+                saludo = "Buenas noches";
+                break;
+              case (hora >= 6 && hora <= 12):
+                saludo = "Buenos días";
+                break;
+              case (hora >= 13 && hora <= 19):
+                saludo = "Buenas tardes";
+                break;
+              case (hora >= 20):
+                saludo = "Buenas noches";
+                break;
+              default:
+                saludo = "default";
+                break;
+            }
+            var pre_message = saludo +"\nQuisiera participar del siguiente curso: " +
+              json_response.titulo;
+
+            var form_message = document.querySelector('#form_message')
+            form_message.value = pre_message;
+
+            document.querySelector("#tipo_consulta").selectedIndex = "1";
+          });
         }
       };
       xhr.open("GET", "getcurso" + "?q=" + boton.dataset.slug, true);
@@ -15,3 +61,9 @@ window.onload = function() {
     });
   });
 };
+
+// MODAL ESCONDIDO
+//<div class="modal" id="modal" style="display: none;" aria-hidden="true">
+
+// MODAL SHOW
+//<div class="modal show" id="modal" style="display: block; padding-right: 15px;" aria-modal="true">
--- lujo/cursos/models.py
+++ lujo/cursos/models.py
@@ -6,7 +6,7 @@ class Curso(models.Model):
     titulo = models.CharField(max_length=100, verbose_name="Título del Curso")
     codigo = models.SlugField(max_length=100, unique=True)
     tema = models.ManyToManyField(Categoria, verbose_name="Temática", null=True, blank=True)
-    resumen = models.TextField(max_length=500, verbose_name="Descripción del Curso")
+    resumen = models.TextField(max_length=1500, verbose_name="Descripción del Curso")
     fecha_hora = models.DateTimeField(auto_now=False, auto_now_add=False, verbose_name="Fecha y hora")
     imagen = models.ImageField(verbose_name="Imagen", upload_to="cursos")
     fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de publicación")
